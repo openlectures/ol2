082c677b5e7b026cc7ebadc3382f89b47c6e85b2
Jethro Kuan <jethrokuan95@gmail.com>
2012-11-13 14:04:15 +0800

    Missed the lesson stuff, changed the position scoping

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,15 @@
 class Lesson < ActiveRecord::Base
   attr_accessible :is_sublesson, :lesson, :position, :topic_id, :user_id
+  #Validations
+  validates_presence_of :lesson, :topic_id, :user_id
+  
+  #Relations
+  belongs_to :topic
+  
+  #Friendly ID
+  extend FriendlyId
+  friendly_id :lesson, use: :slugged
+
+  #Listing
+  acts_as_list, scope: :topic
 end
