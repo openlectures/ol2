c9c265dba1a362beba1eebff924ad9259c1ca94f
Kenneth Lim <kenlimmj@gmail.com>
2012-11-21 11:52:03 +0800

    Updated seeds.rb with more users
    Signed-off-by: Kenneth Lim <kenlimmj@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -98,7 +98,6 @@ Summary.create( summary: "SUMMARY HERE",
               )
 
 #User Creation
-#Jethro
 u = User.new( email: "jethrokuan95@gmail.com",
               password: "openlectures",
               password_confirmation: "openlectures",
@@ -106,8 +105,47 @@ u = User.new( email: "jethrokuan95@gmail.com",
               given_name: "Jethro",
               surname: "Kuan",
               grad_year: "2013",
-              school: "RJC",
+              school: "Raffles Junior College (RJC)",
               role: "admin",
               phone: "82925511",
+            )
+u.save!(validate: false)
+
+u = User.new( email: "kenlimmj@gmail.com",
+              password: "openlectures",
+              password_confirmation: "openlectures",
+              title: "Chief Executive Officer",
+              given_name: "Kenneth",
+              surname: "Lim",
+              grad_year: "2010",
+              school: "NUS High School of Math and Science (NUSHS)",
+              role: "admin",
+              phone: "92335594",
+            )
+u.save!(validate: false)
+
+u = User.new( email: "garyleecf@gmail.com",
+              password: "openlectures",
+              password_confirmation: "openlectures",
+              title: "Chief Academics Officer",
+              given_name: "Gary",
+              surname: "Lee",
+              grad_year: "2010",
+              school: "NUS High School of Math and Science (NUSHS)",
+              role: "admin",
+              phone: "96977054",
+            )
+u.save!(validate: false)
+
+u = User.new( email: "suhangdageek@gmail.com",
+              password: "openlectures",
+              password_confirmation: "openlectures",
+              title: "Chief Designer",
+              given_name: "Hang",
+              surname: "Su",
+              grad_year: "2012",
+              school: "NUS High School of Math and Science (NUSHS)",
+              role: "admin",
+              phone: "82227051",
             )
 u.save!(validate: false)
