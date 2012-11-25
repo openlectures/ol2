716e5f68c0048704cc3801cf8a773024f7d7ecf8
Jethro Kuan <jethrokuan95@gmail.com>
2012-11-26 01:00:32 +0800

    Added changes to forms for SEAB stuff and fixed _seab partial, still missing sortable

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -14,6 +14,7 @@ class TopicsController < ApplicationController
   # GET /topics/1.json
   def show
     @topic = Topic.find(params[:id])
+    @lessons = @topic.lessons
 
     respond_to do |format|
       format.html # show.html.erb
