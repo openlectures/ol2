f528018102f72f06360a3cdfacfd37a49c121251
Jethro Kuan <jethrokuan95@gmail.com>
2012-12-01 14:06:49 +0800

    Moved to vendor

---
+++ app/assets/javascripts/ .js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  return $('.sortable').sortable({
+    axis: 'y',
+    handle: '.handle',
+    items: 'tr',
+    cursor: 'ns-resize',
+    update: function() {
+      return $.post($(this).data('update-url'), $(this).sortable('serialize'));
+    },
+    helper: function(e, tr)
+    {
+        var $originals = tr.children();
+        var $helper = tr.clone();
+        $helper.children().each(function(index)
+        {
+          // Set helper cell sizes to match the original sizes
+          $(this).width($originals.eq(index).width())
+        });
+        return $helper;
+    }
+  });
+});
