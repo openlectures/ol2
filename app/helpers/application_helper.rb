7cb428095c876cbd10a88b976fe1aafdc554bef5
Jethro Kuan <jethrokuan95@gmail.com>
2012-12-04 03:58:32 +0800

    Fixed bootstrap helper failing in heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,20 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def bootstrap_flash
+    flash_messages = []
+    flash.each do |type, message|
+      # Skip Devise :timeout and :timedout flags
+      next if type == :timeout
+      next if type == :timedout
+      type = :success if type == :notice
+      type = :error   if type == :alert
+      text = content_tag(:div, 
+               content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
+               message, :class => "alert fade in alert-#{type}")
+      flash_messages << text if message
+    end
+    flash_messages.join("\n").html_safe
+  end
 end
