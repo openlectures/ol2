c7ce0a5c83bb4b370747fd1c20cacf397205fef3
Jethro Kuan <jethrokuan95@gmail.com>
2013-02-20 18:37:34 +0800

    Initial Try

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,11 @@
 class UserMailer < ActionMailer::Base
   default :from => "openlecturessg@gmail.com"
-  def welcome_message(user)
-    mail :to => user.email, :subject => "Welcome #{user.email} :-)"
+  def report_error(error,email)
+    @error = error;
+    @email = email;
+    mail :to => "jethrokuan95@gmail.com", :subject => "Error Reported!"
+  end
+  def welcome_message(email)
+    mail :to => email, :subject => "Welcome #{email} :-)"
   end
 end
